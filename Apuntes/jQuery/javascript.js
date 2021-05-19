41e56b5563c7c76e136c92d4283a5785679ea2bb
unknown <alejandrojzeballos@gmail.com>
2021-05-18 23:48:37 -0400

    {add}seleccionando con la clase

--- Apuntes/jQuery/javascript.js
+++ Apuntes/jQuery/javascript.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
     //sleccionamos por el tag
     //muetsra el contenido del tag
-    let varBoton = $('button').html();
-    console.log(varBoton);
+    /* let varBoton = $('button').html();
+    console.log(varBoton); */
+
+    //cambia texto del tag
+    //$('button').html("Cambiado desde Jquery");
+
+    //desde la clase    
+    $('.btn').html("Cambiado desde su clase");
 });
