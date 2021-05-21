229cde32a0c9b892e532055a3698a2b80ba21971
unknown <alejandrojzeballos@gmail.com>
2021-05-20 23:52:19 -0400

    {add} cambiando el tamaño, la posicion y el display

--- Apuntes/jQuery/03-eventos.js
+++ Apuntes/jQuery/03-eventos.js
@@ -2,7 +2,32 @@ $(document).ready(function () {
     $('#boton').click(function (e) {
         //evita que haga un envio de datos 
         e.preventDefault();
-        
-        
+        //cambiando una imagen
+        $('img').attr('src', 'imagen2.png');     
     });
+
+    //seleccionan el primero
+    $('#subcontenedor button:nth-child(1)').click(function (e) { 
+        e.preventDefault();
+        $('img').attr('width', '200px');
+    });
+
+    //seleccionando el segundo
+    $('#subcontenedor button:nth-child(2)').click(function (e) { 
+        e.preventDefault();
+        let margen = $('img').css('margin-left');
+        margen = parseInt(margen.replace('px',''));
+        margen = margen +30;
+
+        $('img').css('margin-left', margen + 'px');
+    });
+
+    //seleccionando al tercero
+    $('#subcontenedor button:nth-child(3)').click(function (e) { 
+        e.preventDefault();
+        //quita y pone la clase
+        $('img').toggleClass('d-none');
+    });
+
+
   });
