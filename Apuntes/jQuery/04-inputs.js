22194615bdca71367f03a808464e97063dbaa5da
unknown <alejandrojzeballos@gmail.com>
2021-05-21 00:29:31 -0400

    {add} evitando que el formulario se envie

--- Apuntes/jQuery/04-inputs.js
+++ Apuntes/jQuery/04-inputs.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
     //cuando existan cambios o se suelte la tecla
     $('#texto').keyup(function (e) { 
+        //no sirven el prevent default por que se refresca la pagina con enter o saliendo del input
         $('#saludo').html('Hola '+$('#texto').val());
+    });
+
+    $('form').submit(function (e) { 
+        //evitamos que el form se envie
+        e.preventDefault();
+        //cada vez que se realiza una funciona va y busca a la libreria
     });
 });
