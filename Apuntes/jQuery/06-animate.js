de8277c10e8e3899fe13c780fa80332b4156b6b5
unknown <alejandrojzeballos@gmail.com>
2021-05-22 22:24:10 -0400

    {add} usando callbacks con animate

--- Apuntes/jQuery/06-animate.js
+++ Apuntes/jQuery/06-animate.js
@@ -4,6 +4,17 @@ $(document).ready(function () {
         //podemos pasar la configuracion de la animacion con el tiempo y el px
         $('img').animate({
             'margin-left': '+=100px',
-        }, 2000);
+            'opacity': '0.5',
+            'width': '150px'
+        }, 2000, 
+        function(){
+            //podemos usar callbacks para revertir o añadir mas animaciones
+            $('img').animate({
+                'margin-left': '-=100px',
+                'opacity': '1',
+                'width': '200px'
+            }, 2000);    
+        }
+        );
     });
 });
