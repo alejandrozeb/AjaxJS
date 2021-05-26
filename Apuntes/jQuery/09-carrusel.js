f0bbf5fcdbfe95a02722259278223a55f0503eb5
unknown <alejandrojzeballos@gmail.com>
2021-05-26 00:05:40 -0400

    {add} añadiendo el opciones a bxslider

--- Apuntes/jQuery/09-carrusel.js
+++ Apuntes/jQuery/09-carrusel.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
-    $('.slider').bxSlider();
+$(document).ready(function () {
+  $(".bxslider").bxSlider({
+    //se mueve automaticamente
+    auto: true,
+    //velociadad en milisegundos
+    speed: 100,
+    //agrega mas controles
+    autoControls: true,
+    //onclick se pausa en una imagen
+    stopAutoOnClick: true,
+    //paginador
+    pager: true,
+    //tipo de paginador
+    pagerType: "short",
+    //tamaño del slider
+    // slideWidth: 100
+    //modo de movimiento
+    mode: "vertical",
+    //muestra en forma aleatoria
+    randomStart:true,
+    //del ultimo pasa al primero
+    infiniteLoop: true
   });
+});
