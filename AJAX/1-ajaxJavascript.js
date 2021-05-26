34fcf92711a5242f0f40df4999b32e1f6344dbd3
unknown <alejandrojzeballos@gmail.com>
2021-05-26 18:36:31 -0400

    {add} hola mundo con ajax

---
+++ AJAX/1-ajaxJavascript.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#leer').click(function (e) { 
+        e.preventDefault();
+        $.get("archivo.txt", function (data, textStatus,jqXHR) { 
+            console.log(data);
+            console.log(textStatus);
+            console.log(jqXHR);
+         });
+        
+    });
+});
