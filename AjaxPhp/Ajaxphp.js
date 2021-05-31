2755a5659d8e4e63770c5f18fe445d4b7aea47a2
unknown <alejandrojzeballos@gmail.com>
2021-05-31 17:59:44 -0400

    {add} enviando datos en un json con jq

--- AjaxPhp/Ajaxphp.js
+++ AjaxPhp/Ajaxphp.js
@@ -3,8 +3,14 @@ $(document).ready(function () {
         e.preventDefault();
         let operacion =  $("#operacion").val();
         let texto =  $("#texto").val();
-        $.get(`http://localhost/AjaxPhpJq/AjaxJS/AjaxPhp/convertir.php?operacion=${operacion}&texto=${texto}`, function(data){
+       /*  $.get(`http://localhost/AjaxPhpJq/AjaxJS/AjaxPhp/convertir.php?operacion=${operacion}&texto=${texto}`, function(data){
             console.log(data);
+        }); */
+        //con objeto
+        $.get("http://localhost/AjaxPhpJq/AjaxJS/AjaxPhp/convertir.php", 
+            {"operacion": operacion, 'texto': texto},
+            function (data, textStatus, jqXHR) {
+                $('#listaEmpleados').html(data);    
         });
     });
 });
