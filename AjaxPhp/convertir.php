7cbf2a8e53ced7eb8c843cbc5190e75ef350557e
unknown <alejandrojzeballos@gmail.com>
2021-05-30 23:42:09 -0400

    {add} añadiendo codigo php para backend

---
+++ AjaxPhp/convertir.php
@@ -0,0 +1,9 @@
+<?php
+//atiende de cualquier lugar la peticion
+header('Access-Control-Allow_origin: *');
+if($_REQUEST['operacion'] == 'may'){
+    echo strtoupper($_REQUEST['texto']);
+}else if($_REQUEST['operacion'] == 'min'){
+    echo strtolower($_REQUEST['texto']);
+}
+?>
