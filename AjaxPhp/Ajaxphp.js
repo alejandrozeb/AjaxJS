f39837a4b9736a24ba2bbfc66817221362d85894
unknown <alejandrojzeballos@gmail.com>
2021-05-31 17:54:35 -0400

    {add} enviado el request con ajax

---
+++ AjaxPhp/Ajaxphp.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#convertir').click(function (e) { 
+        e.preventDefault();
+        let operacion =  $("#operacion").val();
+        let texto =  $("#texto").val();
+        $.get(`http://localhost/AjaxPhpJq/AjaxJS/AjaxPhp/convertir.php?operacion=${operacion}&texto=${texto}`, function(data){
+            console.log(data);
+        });
+    });
+});
