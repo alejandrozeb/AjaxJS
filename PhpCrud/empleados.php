95997d295face5cabc5a802de37ad46253abab97
unknown <alejandrojzeballos@gmail.com>
2021-06-02 18:29:44 -0400

    {add} realizando una consulta con php

--- PhpCrud/empleados.php
+++ PhpCrud/empleados.php
@@ -4,10 +4,20 @@ $servidor = 'localhost';
 $usuario = "root";
 $pass = '';
 $bd = "empleados";
-$mysql = new mysqli($servidor, $usuario, $pass, $bd);
+$mysql = new mysqli($servidor, $usuario, $pass, $bd, 3308);
 
 $arreglo = array();
 
-$consulta = "SELECT id,nombre,puesto";
+$consulta = "SELECT id,nombre,puesto, edad FROM empleados";
+
+if($result = $mysql->query($consulta)){
+    while($row = $result->fetch_array(MYSQLI_ASSOC)){
+        $arreglo[]=$row;
+    }
+    echo json_encode($arreglo);
+}
+
+$result->close();
+$mysql->close();
 
 ?>
