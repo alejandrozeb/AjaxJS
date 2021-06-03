15f812237a2bbc67e6b230ad9adef2bed7b6146c
unknown <alejandrojzeballos@gmail.com>
2021-06-02 22:34:57 -0400

    {add} añadiendo la consulta get a empleados bd con ajax

--- PhpCrud/empleados.js
+++ PhpCrud/empleados.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
     $('#leerEmpleados').click(function (e) { 
         e.preventDefault();
-        
+
+        $('#listaEmpleados').html('');
+        $.getJSON("http://localhost/AjaxPhpJq/AjaxJS/PhpCrud/empleados.php",
+            function (data, textStatus, jqXHR) {
+                $.each(data, function (index, item) { 
+                     $('#listaEmpleados').html(
+                        $('#listaEmpleados').html() +     
+                    `
+                     <li> Nombre: ${item.nombre} </li>
+                     <li> Puesto: ${item.puesto} </li>
+                     `);
+                });
+            }
+        );
     });
 });
